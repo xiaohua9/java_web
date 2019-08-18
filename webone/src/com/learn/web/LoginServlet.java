c9e08b7330a481dea0756bab3b2ef2a1fa942693
xiaohua9 <xiaohua0123210@163.com>
2019-08-18 08:42:15 +0800

    简单地用了下过滤器

--- webone/src/com/learn/web/LoginServlet.java
+++ webone/src/com/learn/web/LoginServlet.java
@@ -4,14 +4,12 @@ import com.learn.dao.impl.UserDaoImpl;
 import com.learn.entity.User;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
-@WebServlet()
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
